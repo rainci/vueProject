0ab1214d9e01f931dfbcc8dcbd978c6a9e572c9c
liuyuxi520 <2805242676@qq.com>
2018-08-19 21:24:23 +0800

    modify pro1

---
+++ vuePro1/src/pages/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        {{msg}}
+    </div>
+</template>
+<script>
+export default {
+    name: 'Home',
+    data(){
+        return {
+            msg : '刘雨熙的home'
+        }
+    }
+}
+</script>
+
--- vuePro1/src/router/index.js
+++ vuePro1/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import HelloWorld from '../components/HelloWorld'
+import Home from '../pages/Home'
+
 
 Vue.use(Router)
 
@@ -8,8 +10,19 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      component: HelloWorld,
+      children: [
+        {
+          path: '/home',
+          name: 'Home',
+          component: Home
+        }
+      ]
+    },
+    {
+      path: '/myhome',
+      name: 'Home',
+      component: Home
     }
   ]
 })
