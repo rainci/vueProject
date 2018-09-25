bc9404b88a832420de8ff5dd5abb22a5601b6d8a
liufeifeng <liufeifeng@jtoushou.com>
2018-09-25 19:03:06 +0800

    add store

--- vuePro3/src/components/HelloWorld.vue
+++ vuePro3/src/components/HelloWorld.vue
@@ -1,85 +1,16 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    <h1>hello{{ msg }}</h1>
+    <div v-if="isLogin">
+      welcome {{mobile}}
+      <p @click="logout">logout</p>
+    </div>
+    <p @click='show' v-else>登陆</p>
+    <div class="login" v-if="isSHow">
+      <input type="text" v-model.trim = 'mobile' ><br>
+      <input type="password" v-model.trim = 'password' ><br>
+      <button @click="login">登录</button>
+    </div>
   </div>
 </template>
 
@@ -88,7 +19,32 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      mobile: '150123456789',
+      password: '123456'
+    }
+  },
+  computed: {
+    msg(){
+      return this.$store.state.msg
+    },
+    isSHow(){
+      return this.$store.state.isShowLogin
+    },
+    isLogin(){
+      return this.$store.state.isLogin
+    }
+  },
+  methods: {
+    show(){
+      this.$store.commit('showLogin',true)  
+    },
+    login(){
+      //这边做登录操作，然后登录成功后把个人信息暂存vuex
+      this.$store.commit('loginSuccess',{'mobile':this.mobile,'password':this.password})  
+      this.$store.commit('showLogin',false)  
+    },
+    logout(){
+      this.$store.commit('logout')  
     }
   }
 }
--- vuePro3/src/store/index.js
+++ vuePro3/src/store/index.js
@@ -2,9 +2,28 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 var state = {
-    msg: 'wow vuex'
+    msg: 'wow vuex',
+    mobile: '',
+    password: '',
+    isShowLogin: false,
+    isLogin: false
 };
 Vue.use(Vuex)
 export default new Vuex.Store({
-    state
+    state,
+    mutations: {
+       showLogin(state,flag){
+            state.isShowLogin = flag;
+       },
+       loginSuccess(state,opt){
+           state.mobile= opt.mobile;
+           state.password= opt.password;
+           state.isLogin = true;
+       },
+       logout(state){
+        state.mobile= '';
+        state.password= '';
+        state.isLogin = false; 
+       } 
+    }
 });
